34836fc51900b45ee02d9e3d3afd0c2178e171e7
Nguyen <jn89b@umsystem.edu>
2021-10-21 21:50:59 -0500

    updated drone scripts

--- astar_drones.py
+++ astar_drones.py
@@ -22,10 +22,11 @@ import matplotlib.pyplot as plt
 
 class UAV():
     """this is a fake uav has an id and position"""
-    def __init__(self, id_name , position, goal):
+    def __init__(self, id_name , position, index, goal):
         self.id = id_name
         self.starting_position = position
         self.goalpoint = goal
+        self.zone_index = index
         self.path = None
 
 
@@ -62,7 +63,7 @@ class Astar():
         self.grid = grid
         self.start = start
         self.goal = goal
-        self.collision_bubble = 2
+        self.collision_bubble = 5.0
         
         self.obstacle_list = obs_list
 
@@ -114,7 +115,7 @@ class Astar():
         #while len(self.openset) > 0:
             count = count + 1
             #print(count)
-            if count >= 200:
+            if count >= 2000:
                 print("iterations too much")
                 return self.closedset
             
@@ -266,60 +267,50 @@ def return_other_zones(zones, index):
     copy.pop(index)
     return copy
 
+def return_other_uavs(uavs, uav_index):
+    """return all other zones not assigned to uav to make as a no fly zone"""
+    copy = uavs
+    copy.pop(uav_index)
+    return copy
+
 if __name__ == '__main__':
-    grid_row = 20 #y
-    grid_col = 20 #x
+    grid_row = 100 #y
+    grid_col = 100 #x
     grid = generate_grid(grid_row, grid_col)
     
-    obstacle_list = [(17,5), (5,17)]
+    obstacle_list = [(80,50), (20,40)]
     obstacle_list = add_obstacles(grid, obstacle_list)
     
-    landing_zones = [(10,10), (7,7), (15,15)]
+    landing_zones = [(40,45), (40,60), (60,45), (60,60)]
     plot_landing_zones(landing_zones)
     
-    # #uav0
-    uav_0 = UAV("uav0", [1,1], landing_zones[0])
-    new_obstacle = obstacle_list + return_other_zones(landing_zones[:], 0)
-    grid_copy = grid.copy()
-    new_obstacle = add_obstacles(grid_copy, new_obstacle)
-    astar = Astar(grid_copy, new_obstacle,  uav_0.starting_position, uav_0.goalpoint)
-    uav_0.path = astar.main()
-    plot_path(grid_row, grid_col,uav_0.path, new_obstacle, uav_0.goalpoint)
+    uav_0 = UAV("uav0", [1,1], 1,landing_zones[1])
+    uav_1 = UAV("uav1", [30,0], 0, landing_zones[0])
+    uav_2 = UAV("uav2", [15,0], 2, landing_zones[2])
     
-    # # #uav1
-    uav_1 = UAV("uav1", [10,0], landing_zones[1])
-    new_obstacle = obstacle_list + return_other_zones(landing_zones[:], 1) + uav_0.path 
-    new_obstacle = add_obstacles(grid, new_obstacle)
-    grid_copy = grid.copy()
-    astar = Astar(grid, new_obstacle,  uav_1.starting_position, uav_1.goalpoint)
-    uav_1.path = astar.main()
-    plot_path(grid_row, grid_col,uav_1.path, new_obstacle , uav_1.goalpoint)
+    uav_list = [uav_0, uav_1, uav_2]
+    uav_loc = [uav_0.starting_position, uav_1.starting_position, uav_2.starting_position]
     
-    # #uav 1
-    # new_obstacle = obstacle_list 
-    # new_obstacle = new_obstacle + uav_0.path 
-    # new_obstacle = add_obstacles(grid, new_obstacle)
-    # uav_1 = UAV("uav1", [3,0], landing_zones[1])
-    # astar_2 = Astar(grid, new_obstacle, uav_1.starting_position, uav_1.goalpoint)
-    # uav_1.path = astar_2.main()
-    # plot_path(grid_row, grid_col,uav_1.path, new_obstacle, uav_1.goalpoint)
+    waypoint_dict = {}
+    path_obst = []
+    for idx, uav in enumerate(uav_list):
+        print(idx)
+        if idx == 0:
+            new_obstacle = obstacle_list + return_other_zones(landing_zones[:], uav.zone_index) + return_other_uavs(uav_loc[:], idx)
+        else:
+            #append more than path to uav
+            path_obst.append(uav_list[idx-1].path)
+            flat_list = [item for sublist in path_obst for item in sublist]
+            new_obstacle = obstacle_list + return_other_zones(landing_zones[:], uav.zone_index) + return_other_uavs(uav_loc[:], idx) + flat_list
+            
+        grid_copy = grid.copy()
+        new_obstacle = add_obstacles(grid_copy, new_obstacle)
+        astar = Astar(grid_copy, new_obstacle,  uav.starting_position, uav.goalpoint)
+        uav.path = astar.main()
+        waypoint_dict[uav.id] = uav.path
+        plot_path(grid_row, grid_col, uav.path, new_obstacle , uav.goalpoint) 
     
-    #uav 2
-    # new_obstacle = new_obstacle + uav_1.path
-    # new_obstacle = add_obstacles(grid, new_obstacle)
-    # uav_2 = UAV("uav2", [20,0], landing_zones[2])
-    # astar_2 = Astar(grid, new_obstacle, uav_2.starting_position, uav_2.goalpoint)
-    # uav_2.path = astar_2.main()
-    # plot_path(grid_row, grid_col,uav_2.path, new_obstacle, uav_2.goalpoint)
-    
-    #uav 3
-    # new_obstacle = new_obstacle + uav_2.path
-    # new_obstacle = add_obstacles(grid, new_obstacle)
-    # uav_3 = UAV("uav3", [10,0], landing_zones[3])
-    # astar_3 = Astar(grid, new_obstacle, uav_3.starting_position, uav_3.goalpoint)
-    # uav_3.path = astar_3.main()
-    # plot_path(grid_row, grid_col,uav_3.path, new_obstacle, uav_3.goalpoint)
-    
+
 
     
     
